6f1014e3df67a423e41a531d83c033355cd6307b
Christophe Manin <christophe.manin@rcf.fr>
2026-06-16 16:36:41 +0200

    implémentation de Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   future: {
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   devServer: {
     host: '0.0.0.0',
